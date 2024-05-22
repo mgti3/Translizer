a25a47187508888ad496629bc115c01b7655ea91
ibrahim <ibrahim0ineizeh@gmail.com>
2024-05-22 11:46:27 +0300

    init commit wednesday

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view("home");
+        return view("landing");
     }
 
     public function adminTeamManagement(): string
