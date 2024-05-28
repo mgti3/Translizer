4d642b47767286458e55c8695bfd35d487ba3ffb
ibrahim <ibrahim0ineizeh@gmail.com>
2024-05-28 12:04:27 +0300

    added missing routing

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -39,7 +39,7 @@ $routes->get('logout', 'Home::logout');
 
 
 // Admin Routes (assuming admin user type is 1)
-$routes->group('', ['filter' => 'auth:0'], function($routes) {
+$routes->group('', ['filter' => 'auth:0'], function ($routes) {
     $routes->add('admin_dashboard', 'Admin::dashboard');
     $routes->add('admin_employees_management', 'Admin::adminEmployeesManagement');
     $routes->post('admin_employees_management', 'Admin::addEmployee');
@@ -47,14 +47,14 @@ $routes->group('', ['filter' => 'auth:0'], function($routes) {
 });
 
 // Manager Routes (assuming manager user type is 2)
-$routes->group('', ['filter' => 'auth:4'], function($routes) {
+$routes->group('', ['filter' => 'auth:4'], function ($routes) {
     $routes->add('manager_dashboard', 'Manager::dashboard');
     $routes->add('manager_tickets', 'Manager::ticket');
     $routes->add('manager_assignment', 'Manager::assignment');
 });
 
 // Employee Routes (assuming employee user type is 3)
-$routes->group('', ['filter' => 'auth:1'], function($routes) {
+$routes->group('', ['filter' => 'auth:1'], function ($routes) {
     $routes->add('employee_dashboard', 'Employee::dashboard');
     $routes->add('employee_orderHistory', 'Employee::orderHistory');
     $routes->add('employee_orderDetails', 'Employee::orderDetails');
@@ -64,9 +64,10 @@ $routes->group('', ['filter' => 'auth:1'], function($routes) {
 });
 
 // User Routes (assuming general user type is 4)
-$routes->group('', ['filter' => 'auth:2'], function($routes) {
+$routes->group('', ['filter' => 'auth:2'], function ($routes) {
     $routes->add('user_dashboard', 'User::dashboard');
     $routes->add('orders_page', 'User::orders');
     $routes->add('reports_page', 'User::reports');
     $routes->add('user_viewTranslation', 'User::user_viewTranslation');
+    $routes->add('submit', 'User::submitOrder');
 });
