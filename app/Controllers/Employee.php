aa5d0728d03f280858bff307289d426d72287aa2
Mahmoud <my8977907@example.com>
2024-05-30 13:39:10 +0300

    employee dashboard data is all dynamic now 30/5

--- app/Controllers/Employee.php
+++ app/Controllers/Employee.php
@@ -1,12 +1,46 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\addEmployeeModel;
+use App\Models\OrderSubmission_model;
+
+
 
 class Employee extends BaseController
 {
+    protected $employeeModel;
+
+    public function __construct()
+    {
+        $this->employeeModel = new addEmployeeModel();
+        $this->OrderSubmit = new OrderSubmission_model();
+
+    }
+
     public function dashboard(): string
     {
-        return view("employee_dashboard");
+        $session = \Config\Services::session();
+        $user_id = $session->get('user_id');
+        $username= $session->get('username');
+
+
+        $data = [
+            'orders' => 0,
+            'progress' => 0,
+            'completed' => 0,
+            'ordersCount' => 0,
+            'rating' => 0,
+            'username' => $username
+        ];
+        $data['completed'] = $this->OrderSubmit->where('employee_id', $user_id)
+                           ->where('state', 'done')
+                           ->countAllResults();
+        $data['ordersCount'] = $this->OrderSubmit->where('employee_id', $user_id)->countAllResults();
+        $data['orders'] = $this->OrderSubmit->where('employee_id', $user_id)->findAll();
+        $data['progress'] = floor(($data['completed'] / $data['ordersCount']) * 100);
+
+
+        return view("employee_dashboard",$data);
     }
 
     public function orderHistory(): string
